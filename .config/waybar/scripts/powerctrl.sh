2f454b5be6335bed585c748cc3e3ae0c9ad8b95f
Jonas Meyer-Ohle <jonasmo@gmail.com>
2022-01-23 18:06:37 +0100

    Changed foot & waybar configs

--- .config/waybar/scripts/powerctrl.sh
+++ .config/waybar/scripts/powerctrl.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 case $(wofi -d -L 6 -l 3 -W 100 -x -100 -y 10 \
-    -D dynamic_lines=true  << EOF | sed 's/^ *//'
+    -D dynamic_lines=false  << EOF | sed 's/^ *//'
+    Lock
     Shutdown
     Reboot
     Log off
-    Lock
+    Reboot to Windows
     Cancel
 EOF
 ) in
@@ -19,5 +20,8 @@ EOF
         ;;
     "Log off")
         swaymsg exit
+        ;;
+    "Reboot to Windows")
+        systemctl reboot --boot-loader-entry=auto-windows
         ;;
 esac
